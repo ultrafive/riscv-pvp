f392be134ba7a048e3e9c250fa042157a80df77f
Tianliang.Nan <Tianliang.Nan@streamcomputing.com>
2021-04-16 10:12:29 +0800

    update mask load insn and golden data

--- isa/rvv/vse32_v.py
+++ isa/rvv/vse32_v.py
@@ -1,5 +1,6 @@
 from isa.inst import *
 import numpy as np
+import math
 
 class Vse32_v(Inst):
     name = 'vse32.v'
@@ -9,14 +10,13 @@ class Vse32_v(Inst):
             return self['rs1']
         else:
             mask_list = []
-            if 'vlen' in self:
-                v0_mask = self['mask']
-                for index in range(0, self['vlen']):
+            for index in range(0, math.ceil(self['vlen'] / 8 )):
+                v0_mask = self['mask'][index]
+                for shift in range(0, 8):
                     if v0_mask & 1 == 0:
                         mask_list.append(True)
                     else:
                         mask_list.append(False)
-
                     v0_mask = v0_mask >> 1
 
-            return np.ma.array(self['rs1'], mask = mask_list, fill_value = 0).filled()
+            return np.ma.array(self['rs1'], mask = mask_list[0: self['vlen']], fill_value = 0).filled()
