49c6dccb4721db933016e1aa395753c76595655a
Tianliang.Nan <Tianliang.Nan@streamcomputing.com>
2021-04-17 20:17:23 +0800

    add vlsx.v and vssx.v insns case

--- isa/rvv/__init__.py
+++ isa/rvv/__init__.py
@@ -11,3 +11,13 @@ from .vse8_v import *
 from .vse16_v import *
 from .vse32_v import *
 from .vse64_v import *
+
+from .vlse8_v import *
+from .vlse16_v import *
+from .vlse32_v import *
+from .vlse64_v import *
+
+from .vsse8_v import *
+from .vsse16_v import *
+from .vsse32_v import *
+from .vsse64_v import *
