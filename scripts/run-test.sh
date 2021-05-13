b441c8539706f8af686a65d5d8ed8ce5e1c0cc49
Xin Ouyang <xin.ouyang@streamcomputing.com>
2021-05-13 14:40:51 +0800

    fix run tests scripts.

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -4,4 +4,4 @@ script_dir=`dirname $0`
 
 . $script_dir/env.common
 
-rm -rf output && pytest --alluredir=output --basetemp=$PWD/build -v $* run.py
+rm -rf output && python run.py --nproc `nproc` $*
