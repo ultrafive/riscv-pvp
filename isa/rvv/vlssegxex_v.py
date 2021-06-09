1aebfef5460575f9c1337b73913b633c3c023326
Xin Ouyang <xin.ouyang@streamcomputing.com>
2021-06-10 00:21:57 +0800

    fix vlsseg* with lmul and more cases.

--- isa/rvv/vlssegxex_v.py
+++ isa/rvv/vlssegxex_v.py
@@ -1,26 +1,18 @@
 from isa.inst import *
 import numpy as np
 import math
-
 class Vlssegxex_v(Inst):
     name = 'vlssegxex.v'
 
     def golden(self):
+        nf = self['nfields']
+        vlen = self['vlen']
+        stride = int(self['stride']/self['rs1'].itemsize)
+        assert int(self['rs1'].size / stride) == vlen
+        assert stride >= nf
 
-        vl = self['rs1'].size/(self['rs2']/self['rs1'].itemsize)
-        vd_size = vl * self['nfields']
-        vd = np.zeros( int(vd_size), dtype=self['rs1'].dtype )
-        for no in range(0, int(vl)):
-            for idx in range(0, self['nfields']):
-                vd[ int(self['nfields']*no+idx) ] = self['rs1'][int((self['rs2']/self['rs1'].itemsize)*no+idx)]
+        vlmax = int(self['lmul'] * self['VLEN'] / (self['rs1'].itemsize * 8))
 
-        if 'mask' in self:
-            mask = []
-            for no in range(0,int(vl)):
-                mask = ( self['mask'][np.floor(no/8).astype(np.int8)] >> (no % 8) ) & 1
-                if mask != 1:
-                    for idx in range( 0, self['nfields'] ):
-                        vd[ int(self['nfields']*no+idx) ] = self['orig'][int(self['nfields']*no+idx)]
-            return vd
-        else:
-            return vd
+        vds = self.masked(self['rs1'].reshape((vlen, stride))[:,:nf].T)
+        vds = np.pad(vds, [(0,0),(0, int(vlmax - vlen))])
+        return vds.reshape((vlmax * nf))
