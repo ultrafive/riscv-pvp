efd216e4ded4c90afea0eb9e33277edf5cab7243
Xin Ouyang <xin.ouyang@streamcomputing.com>
2021-06-10 10:30:52 +0800

    fix vluxseg*/vloxseg* with lmul and more cases.

--- isa/rvv/vloxsegxeix_v.py
+++ isa/rvv/vloxsegxeix_v.py
@@ -6,20 +6,12 @@ class Vloxsegxeix_v(Inst):
     name = 'vloxsegxeix.v'
 
     def golden(self):
-        vl = self['vs2'].size
-        vd_size = vl * self['nfields']
-        vd = np.zeros( vd_size, dtype=self['rs1'].dtype )
-        for no in range( 0, self['vs2'].size ):
-            for idx in range(0, int(self['nfields']) ):
-                vd[int( no*self['nfields']+idx )] = self['rs1'][int( self['vs2'][no]/self['rs1'].itemsize +idx )]
+        nf = self['nfields']
+        vlen = self['vlen']
+        idx = (self['idx'] / self['rs1'].itemsize).astype(np.int)
 
-        if 'mask' in self:
-            mask = []
-            for no in range( 0, vl ):
-                mask = ( self['mask'][np.floor(no/8).astype(np.int8)] >> (no % 8) ) & 1
-                if mask != 1:
-                    for idx in range( 0, self['nfields'] ):
-                        vd[ int(self['nfields']*no+idx) ] = self['orig'][int(self['nfields']*no+idx)]
-            return vd
-        else:
-            return vd
+        vlmax = int(self['lmul'] * self['VLEN'] / (self['rs1'].itemsize * 8))
+
+        vds = self.masked(np.take(self['rs1'], idx[None].T + np.arange(nf)).T)
+        vds = np.pad(vds, [(0,0),(0, int(vlmax - vlen))])
+        return vds.reshape((vlmax * nf))
