9d1e0e83002e246600a1cffd3d1f123979912eb6
kening.zhang <kening.zhang@streamcomputing.com>
2021-06-28 21:08:22 +0800

    add vid.v mask

--- isa/rvv/vid_v.py
+++ isa/rvv/vid_v.py
@@ -6,4 +6,8 @@ class Vid_v(Inst):
     name = 'vid.v'
 
     def golden(self):
-        return np.arange(self['vlen']).astype('uint'+str(self['ebits']))
+        res = np.arange(self['vlen']).astype('uint'+str(self['ebits']))
+        if 'mask' in self:
+            mask = np.unpackbits(self['mask'], bitorder='little')[0:self['vlen']]
+            res = np.where(mask==1, res, 0)
+        return res
