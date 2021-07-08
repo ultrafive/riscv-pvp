3fe98f7c1784e34418b99fbf1af133b730161e89
kening.zhang <kening.zhang@streamcomputing.com>
2021-07-08 14:50:44 +0800

    add vlexff.v cases

--- isa/rvv/vlexff_v.py
+++ isa/rvv/vlexff_v.py
@@ -6,11 +6,19 @@ class Vlexff_v(Inst):
     name = 'vlexff.v'
 
     def golden(self):
-
-        vd = self['rs1'].copy()
-
-        return self.masked( vd, self['orig'] if 'orig' in self else 0 )
-
+        res = np.linspace(1, 0x400, 0x400, dtype=np.uint8)
+        if self['ebits'] == 16:
+            res.dtype = np.uint16
+        elif self['ebits'] == 32:
+            res.dtype = np.uint32
+        elif self['ebits'] == 64:
+            res.dtype = np.uint64
+            
+        if 'nvl' not in self:
+            res = res[0:self['vlen']]
+            return self.masked(res)
+        else:
+            return res[0-self['nvl']-1:-1]
 
 class Vle8ff_v(Vlexff_v):
     name = 'vle8ff.v'
