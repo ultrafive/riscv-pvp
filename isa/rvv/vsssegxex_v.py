5aa8c5cecf16a56cd7158fc34b31ca335e5682f5
kening.zhang <kening.zhang@streamcomputing.com>
2021-08-15 15:56:38 +0800

    fix stride segment load/store

--- isa/rvv/vsssegxex_v.py
+++ isa/rvv/vsssegxex_v.py
@@ -13,14 +13,14 @@ class Vsssegxex_v(Inst):
             if self['isExcept'] > 0:
                 return np.zeros(1024*8//(self['vs3'].itemsize*8), dtype=self['vs3'].dtype)
         nf = self['nf']
-        vlen = self['vlen']
+        vl = self['vl']
         emul = self['eew'] /self['sew']*factor_lmul[self['lmul']]
         emul = 1 if emul < 1 else int(emul)
         vlmax = int(emul * self['VLEN'] // (self['vs3'].itemsize*8))
         eew = self['eew']//8
         stride = self['rs2']//eew
 
-        vs3 = self['vs3'][0: vlmax*nf].reshape(nf, vlmax)[:, 0:vlen]
+        vs3 = self['vs3'][0: vlmax*nf].reshape(nf, vlmax)[:, 0:vl]
 
         if 'start' in self:
             start = self['start']
@@ -33,19 +33,19 @@ class Vsssegxex_v(Inst):
             origin = np.zeros(self['VLEN']*8//(self['vs3'].itemsize*8), dtype=self['vs3'].dtype)
         
         if 'mask' in self:
-            mask = np.unpackbits(self['mask'], bitorder='little')[0: self['vlen']]
+            mask = np.unpackbits(self['mask'], bitorder='little')[0: self['vl']]
         else :
-            mask = np.ones(self['vlen'], dtype=np.uint8)
+            mask = np.ones(self['vl'], dtype=np.uint8)
 
         if self['rs2'] >= eew*nf:
             for i in range(nf):
-                origin[start*nf+i: vlen*nf+i: nf] = self.masked(vs3[i], origin[i: vlen*nf+i: nf])[start: vlen]
+                origin[start*nf+i: vl*nf+i: nf] = self.masked(vs3[i], origin[i: vl*nf+i: nf])[start: vl]
         else:
             tmp = self['origin'] if 'origin' in self else np.zeros(self['VLEN']*8//(self['vs3'].itemsize*8), dtype=self['vs3'].dtype)
-            for i in range(start, vlen):
+            for i in range(start, vl):
                 if mask[i] != 0:
                     tmp[i*stride: i*stride+nf] = vs3[:,i]
-            for i in range(vlen):
+            for i in range(vl):
                     origin[i*nf: i*nf+nf] = tmp[i*stride: i*stride+nf]
 
         return origin
